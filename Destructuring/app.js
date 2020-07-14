7c68b8b0a6a4c93fb874f6ee351b2aa7f91b44b5
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-07-14 21:14:13 +0500

    Add files via upload

---
+++ Destructuring/app.js
@@ -0,0 +1,139 @@
+// // ******************
+// // Examples 1
+
+// let a, b, rest;
+// [a, b] = [10, 20];
+
+// console.log(a);
+// // expected output: 10
+
+// console.log(b);
+// // expected output: 20
+
+// [a, b, ...rest] = [1, 2, 30, 40, 50, 60];
+
+// console.log(rest);
+// expected output: Array [30,40,50]
+
+// // ******************
+// // Examples 2
+// // Swapping variables
+
+
+// let a = 1;
+// let b = 3;
+
+// [a, b] = [b, a];
+// console.log(a); // 3
+// console.log(b); // 1
+
+// const arr = [1,2,3];
+// console.log(arr);
+// [arr[2], arr[1]] = [arr[1], arr[2]];
+// console.log(arr); // [1,3,2]
+
+
+
+// // ******************
+// // Examples 3
+// // Parsing an array returned from a function
+
+
+// function f() {
+//     return [1, 2, 3, 4];
+//   }
+  
+//   let a, b; 
+//   [a, b, c] = f(); 
+//   console.log(a); // 1
+//   console.log(b); // 2
+
+
+// // ******************
+// // Examples 4
+// // Ignoring some returned values
+
+// function f() {
+//     return [1, 2, 3];
+//   }
+  
+//   const [a, , b] = f();
+//   console.log(a); // 1
+//   console.log(b); // 3
+
+// // ******************
+// // Examples 5
+
+// // Assigning the rest of an array to a variable
+
+// const [a, ...b] = [1, 2, 3];
+// console.log(a);                 // 1
+// console.log(b);                 // [2, 3]
+
+
+// // ******************
+// Examples 6
+
+// Object destructuring
+
+// const user = {
+//     id: 42,
+//     is_verified: true,
+//     name1: "Arsalan"
+// };
+
+// const {id, is_verified} = user;
+
+// console.log(id); // 42
+// console.log(is_verified); // true 
+// console.log(name);
+
+
+// // ******************
+// // Examples 7
+
+// Assignment without declaration
+
+// let a, b;
+
+// ({a, b} = {a: 1, b: 2});
+
+
+// ******************
+// Examples 8
+
+
+// // Assigning to new variable names
+
+// const o = {p: 42, q: true};
+// const {p: foo, q: bar} = o;
+ 
+// console.log(foo); // 42 
+// console.log(bar); // true
+
+
+// // ******************
+// // Examples 9
+
+// Default values
+
+
+// const {a = 10, b = 5} = {a: 3};
+
+// console.log(a); // 3
+// console.log(b); // 5
+
+
+// // ******************
+// // Examples 10
+
+
+
+// // ******************
+// // Examples 11
+
+
+
+
+// // ******************
+// // Examples 12
---
+++ comments.js
@@ -0,0 +1,11 @@
+// Single Line Comments
+
+/*
+
+MultiLine 
+Comments
+
+*/
+
+var str1 = "Comments Examples"
+console.log(str1);
