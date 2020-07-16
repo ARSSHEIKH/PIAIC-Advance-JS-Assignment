1da45654728522b3125d381d9103ed9e18b212ab
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-07-17 00:39:15 +0500

    Updated both files

--- Form/app.js
+++ Form/app.js
@@ -30,18 +30,21 @@ function signup_func1(){
     document.querySelector(".signup-form3").innerHTML="";
 
     if(filled == true){
-        document.getElementById('fname').value = UsersArr[ind-1].fname;
-        document.getElementById('lname').value = UsersArr[ind-1].lname;
-        document.getElementById('email').value = UsersArr[ind-1].email
-        document.getElementById('confemail').value = UsersArr[ind-1].confemail
-        document.getElementById('pass').value = UsersArr[ind-1].pass
-        document.getElementById('confpass').value = UsersArr[ind-1].confpass
+        document.getElementById('fname').value = UsersArr1[ind-1].fname;
+        document.getElementById('lname').value = UsersArr1[ind-1].lname;
+        document.getElementById('email').value = UsersArr1[ind-1].email
+        document.getElementById('confemail').value = UsersArr1[ind-1].confemail
+        document.getElementById('pass').value = UsersArr1[ind-1].pass
+        document.getElementById('confpass').value = UsersArr1[ind-1].confpass
+        document.getElementById("dvOne-tab").style.backgroundColor = "#add7a8";
+        document.getElementById("dvOne-tab").style.border = "3px solid  #add7a8";    
+
     }
 }
 
 function signup_func2(){
     document.getElementById("dvOne-tab").style.backgroundColor = "#fff";
-    document.getElementById("dvOne-tab").style.border = "3px solid #ff7b00";
+    document.getElementById("dvOne-tab").style.border = "3px solid #ebf3eb";
     document.getElementById("dvThree-tab").style.backgroundColor = "#fff";
     document.getElementById("dvThree-tab").style.border = "3px solid #ebf3eb";
     document.getElementById("tabs-strings").style.marginTop = "12px";
@@ -52,6 +55,10 @@ function signup_func2(){
     document.querySelector(".signup-form1").innerHTML = signup_content2;
     document.querySelector('.signup-form2').innerHTML="";
     document.querySelector(".signup-form3").innerHTML="";
+    if(filled == true){
+        document.getElementById("dvOne-tab").style.backgroundColor = "#add7a8";
+        document.getElementById("dvOne-tab").style.border = "3px solid  #add7a8";    
+    }
 }
 
 function signup_func3(){
@@ -67,7 +74,7 @@ function signup_func3(){
     document.querySelector('.signup-form2').innerHTML="";
     document.querySelector(".signup-form3").innerHTML="";
 }
-function Users(fname, lname, email, confemail, pass, confpass) {
+function Users_Form1(fname, lname, email, confemail, pass, confpass) {
     this.fname = fname;
     this.lname = lname;
     this.email = email;
@@ -76,11 +83,24 @@ function Users(fname, lname, email, confemail, pass, confpass) {
     this.confpass = confpass;
 
 }
+function Users_Form2(NearestOff, address, address2, city, state, country, postalcode, phone, mphone) {
+    this.NearestOff = NearestOff;
+    this.address = address;
+    this.address2 = address2;
+    this.city = city;
+    this.state = state;
+    this.country = country;
+    this.postalcode = postalcode;
+    this.phone = phone;
+    this.mphone = mphone;
+
+}
 // var arr = [10]
 // arr[0] = new Students("Anam", 20, 'karachi...')
 
 // console.log(arr[0])
-var UsersArr = [];
+var UsersArr1 = [];
+var UsersArr2 = [];
 var ind = 0;
 let fname;
 let lname;
@@ -88,8 +108,20 @@ let email;
 let confemail;
 let pass ;
 let confpass;
+
+let NearestOff;
+let address;
+let address2;
+let city;
+let state;
+let country;
+let postalcode;
+let phone;
+let mphone;
+
 let filled = false;
-UsersArr[ind] = new Users();
+UsersArr1[ind] = new Users_Form1();
+UsersArr2[ind] = new Users_Form2();
 
 function submit_Form1(){
     fname = document.getElementById('fname').value
@@ -98,21 +130,44 @@ function submit_Form1(){
     confemail = document.getElementById('confemail').value
     pass = document.getElementById('pass').value
     confpass = document.getElementById('confpass').value
+
     filled = false;
 
     if((fname != "" && lname != "" && email != "" && confemail != "" && pass != "" && confpass != "" ) && (email === confemail) && (pass === confpass)){
 
-        UsersArr[ind].fname = fname;
-        UsersArr[ind].lname = lname;
-        UsersArr[ind].email = email;
-        UsersArr[ind].confemail = confemail;
-        UsersArr[ind].pass = pass;
-        UsersArr[ind].confpass = confpass;
+        UsersArr1[ind].fname = fname;
+        UsersArr1[ind].lname = lname;
+        UsersArr1[ind].email = email;
+        UsersArr1[ind].confemail = confemail;
+        UsersArr1[ind].pass = pass;
+        UsersArr1[ind].confpass = confpass;
+        filled = true;
         signup_func2();
         document.getElementById("dvOne-tab").style.backgroundColor = "#add7a8";
         document.getElementById("dvOne-tab").style.border = "3px solid  #add7a8";    
-        filled = true;
+
+
+        submit_Form2();
+        // function submit_Form2(){
+        //     NearestOff = document.getElementById('NearestOff').value
+        //     address = document.getElementById('address').value
+        //     address2 = document.getElementById('address2').value
+        //     confemcityail = document.getElementById('city').value
+        //     state = document.getElementById('state').value
+        //     country = document.getElementById('country').value
+        //     postalcode = document.getElementById('postalcode').value
+        //     phone = document.getElementById('phone').value
+        //     mphone = document.getElementById('mphone').value
+
+        //     if(NearestOff != "Nearest Office" && address != "" && address2  != address && city != "" && state != "State" && country != "Country" && postalcode != "" && phone != "" && mphone != phone)
+        //     {
+        //         console.log("abcd")
+        //     }
+        // }
+        
         ind++;
+
+
 
     }
     else if(email !== confemail){
